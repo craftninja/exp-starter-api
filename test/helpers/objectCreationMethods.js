cab662a3a33cf393d9153a6034050156b99721bd
kyle.mickey <kyle.mickey@cognizant.com>
2017-10-04 14:39:00 -0600

    Updated createUser test to use random 4 digit sequence

--- test/helpers/objectCreationMethods.js
+++ test/helpers/objectCreationMethods.js
@@ -1,11 +1,25 @@
 const User = require('../../models/user');
 
+const randomDigits = () => {
+  const pad = '0000';
+  const num = Math.floor(Math.random() * 9999);
+  return (pad + num).slice(-4);
+};
+
+const randomYear = () => {
+  const min = 1900;
+  const max = new Date().getFullYear();
+  return Math.floor(Math.random() * (max - min)) + min;
+};
+
 exports.createUser = async overrides => {
+  const randomId = randomDigits();
+
   const defaults = {
-    firstName: 'Elowyn',
-    lastName: 'Platzer Bartel',
-    email: 'elowyn@example.com',
-    birthYear: 2015,
+    firstName: `Elowyn${randomId}`,
+    lastName: `Platzer Bartel${randomId}`,
+    email: `elowyn${randomId}@example.com`,
+    birthYear: randomYear(),
     student: true,
     password: 'password',
   };
