c60da6ae4b9b090afd1e17c30ac74311fbe315bf
kyle.mickey <kyle.mickey@cognizant.com>
2017-10-11 15:47:22 -0600

    Spec builder is functioning

--- specBuilder.js
+++ specBuilder.js
@@ -11,42 +11,55 @@ const fs = require('fs');
 const path = require('path');
 
 const specPath = path.resolve(__dirname, './spec');
-const specYaml = path.resolve(specPath, )
-const tmpYaml = {};
+const specYamlPath = path.resolve(specPath, 'spec.yaml');
 
 const readdir = promisify(fs.readdir);
 const readFile = promisify(fs.readFile);
 const stat = promisify(fs.stat);
 
-const throwErr = err => { throw err; };
+const throwErr = err => {
+  process.stderr().write(`${JSON.stringify(err)}`);
+  process.exit(1);
+};
 
-function concatSpecFiles(dir) {
-  readdir(dir)
-    .then(itemNames => {
-      itemNames.forEach(name => {
-        if (name.endsWith('.yml') || name.endsWith('.yaml')) {
-          const filePath = path.resolve(specPath, dir, name);
-          readFile(filePath)
-            .then(data => {
-              tempYaml[dir] = tempYaml[dir] ? tempYaml[dir] + data : data;
-            })
-            .catch(throwErr);
-        }
-      });
-    });
+async function concatSpecFiles(dir) {
+  const dirPath = path.resolve(specPath, dir);
+  const itemNames = await readdir(dirPath);
+
+  return Promise.all(itemNames.map(name => {
+    const filePath = path.resolve(dirPath, name);
+    return name.endsWith('.yml') || name.endsWith('.yaml')
+      ? readFile(filePath)
+        .then(data => ({ [dir]: data }))
+        .catch(throwErr)
+      : Promise.resolve();
+  }));
 }
 
 readdir(specPath)
   .then(itemNames => {
-    itemNames.forEach(name => {
-      const itemPath = path.resolve(specPath, name);
-      stat(itemPath)
-        .then(stats => {
-          if (stats.isDirectory()) {
-            concatSpecFiles(name);
-          }
-        })
-        .catch(throwErr);
-    })
+    return Promise.all(
+      itemNames.map(name => {
+        const itemPath = path.resolve(specPath, name);
+        return stat(itemPath)
+          .then(stats => {
+            return stats.isDirectory()
+              ? concatSpecFiles(name)
+              : Promise.resolve();
+          })
+          .catch(throwErr);
+      }));
+  })
+  .then(ymlResults => {
+    ymlResults = [].concat.apply([], ymlResults);
+    const specObj = yaml.safeLoad(fs.readFileSync(specYamlPath));
+    ymlResults
+      .filter(yml => yml)
+      .forEach(yml => {
+        Object.keys(yml).forEach(key => {
+          specObj[key] = yaml.safeLoad(yml[key].toString());
+        });
+      });
+    fs.writeFileSync(path.resolve(specPath, 'spec.json'), JSON.stringify(specObj, undefined, 2));
   })
   .catch(throwErr);
