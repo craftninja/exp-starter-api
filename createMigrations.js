de936fba7b8d8ad02003dd4d16bc6002c2530501
P P <428873@cognizant.com>
2018-02-15 18:55:06 +0530

    adding migration script generator. updating script in package.json

---
+++ createMigrations.js
@@ -0,0 +1,75 @@
+const fs = require('fs');
+
+const config = {
+  folder: './migrations/',
+  scriptTypes: ['do', 'undo'],
+  fileNameTemplate: '{timeStamp}.{scriptType}.{migrationName}.sql',
+  timeStampTemplate: '{year}{month}{day}{hour}{minute}{second}',
+};
+
+/* eslint-disable no-console */
+const createMigrations = () => {
+  const rawMigrationName = process.argv[2] || '';
+
+  if (rawMigrationName) {
+    const migrationName = rawMigrationName
+      .replace(/^./, str => str.toLowerCase())
+      .replace(/\s+./g, str => str.trim().toUpperCase());
+
+    const timeStamp = getTimeStamp();
+    createMigrationScripts(migrationName, timeStamp);
+  } else {
+    console.log('Invalid migratin script name!');
+  }
+};
+
+const createMigrationScripts = (migrationName, timeStamp) => {
+  const scriptNameTemplateMap = {
+    timeStamp: timeStamp,
+    migrationName: migrationName
+  };
+
+  for (let i = 0; i < config.scriptTypes.length; i++) {
+    scriptNameTemplateMap.scriptType = config.scriptTypes[i];
+    const refinedMigrationName = formatTemplate(scriptNameTemplateMap, config.fileNameTemplate);
+    const migrationFileName = `${config.folder}${refinedMigrationName}`;
+    createFile(migrationFileName);
+  }
+};
+
+const createFile = fileName => {
+  fs.writeFile(fileName, '', (error) => {
+    if (error) {
+      console.log(error);
+    }
+    console.log(`Migration script ${fileName} created!`);
+  });
+};
+
+const getTimeStamp = () => {
+  const currentDate = new Date();
+  const timeStampTemplateMap = {
+    year: currentDate.getFullYear(),
+    month: round(currentDate.getMonth() + 1),
+    day: round(currentDate.getDate()),
+    hour: round(currentDate.getHours()),
+    minute: round(currentDate.getMinutes()),
+    second: round(currentDate.getSeconds()),
+  };
+  return formatTemplate(timeStampTemplateMap, config.timeStampTemplate);
+};
+
+const round = (number) => {
+  return Number(number) < 10 ? `0${number}` : number;
+};
+
+const formatTemplate = (templateMap, template) => {
+  var refinedScriptName = template
+    .replace(/{(.*?)}/g, (match, group) => {
+      return templateMap[group];
+    });
+  return refinedScriptName;
+};
+
+createMigrations();
+/* eslint-enable no-console */
